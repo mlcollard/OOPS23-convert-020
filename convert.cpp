86e84e8492c732d66814c999006d979ce3e14685
Michael L. Collard <collard@uakron.edu>
2023-02-23 08:58:42 -0500

    Initial commit

---
+++ convert.cpp
@@ -0,0 +1,60 @@
+/*
+    convert.cpp
+
+    Convert the text
+
+    Note: The "@concerns" are for pedagogical purposes only.
+
+    * @concerns are assume to be [in], meaning they are predefined and used in the code
+    * @concerns marked [out] meaning they are produced in that section
+*/
+
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int main(int argc, char* argv[]) {
+
+    // requires conversion option and string
+    // @concerns
+    if (argc != 3) {
+        std::cerr << "usage: " << argv[0] << " <option> <string>\n";
+        return 1;
+    }
+
+    // conversion option
+    // @concerns
+    std::string option(argv[1]);
+
+    // input text
+    // @concerns
+    std::string text(argv[2]);
+
+    // convert the string according to the option
+    // @concerns
+    // @concerns
+    // @concerns
+    if (option == "--upper") {
+
+        for (auto pc = text.begin(); pc != text.end(); ++pc)
+            *pc = std::toupper(*pc);
+
+    } else if (option == "--lower") {
+
+        for (auto pc = text.begin(); pc != text.end(); ++pc)
+            *pc = std::tolower(*pc);
+
+    } else {
+
+        std::cerr << "Invalid conversion option: " << option << '\n';
+        return 1;
+    }
+
+    // output converted text
+    // @concerns
+    for (auto pc = text.cbegin(); pc != text.cend(); ++pc)
+        std::cout << *pc;
+    std::cout << '\n';
+
+    return 0;
+}
